bf91b18fc6abb190b79bb0a47a2384d967f9ac95
hanamaru5626-art <hanamaru5626@gmail.com>
2026-02-14 12:51:28 +0900

    Update net.lua

--- net.lua
+++ net.lua
@@ -2,28 +2,30 @@ local net = {}
 
 local modem = peripheral.find("modem")
 local MAIN_CHANNEL = 0
-local myChannel
+local myChannel = nil
 
 function net.init(channel)
-    myChannel = channel
-    modem.open(myChannel)
+  myChannel = channel
+  modem.open(myChannel)
 end
 
 function net.send(to, data)
-    modem.transmit(MAIN_CHANNEL, myChannel, {
-        to = to,
-        from = myChannel,
-        data = data
-    })
+  if not myChannel then error("net not initialized") end
+  modem.transmit(MAIN_CHANNEL, myChannel, {
+    to = to,
+    from = myChannel,
+    data = data
+  })
 end
 
 function net.receive()
-    while true do
-        local _, _, ch, reply, msg = os.pullEvent("modem_message")
-        if ch == myChannel and type(msg) == "table" then
-            return msg.from, msg.data
-        end
+  if not myChannel then error("net not initialized") end
+  while true do
+    local _, _, ch, reply, msg = os.pullEvent("modem_message")
+    if reply == myChannel and type(msg) == "table" then
+      return msg.from, msg.data
     end
+  end
 end
 
 return net
