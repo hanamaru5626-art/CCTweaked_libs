5b41797e2ac81af24030ae61fbb74af0fecf0129
hanamaru5626-art <hanamaru5626@gmail.com>
2026-02-14 12:55:26 +0900

    Update net.lua

--- net.lua
+++ net.lua
@@ -5,12 +5,17 @@ local MAIN_CHANNEL = 0
 local myChannel = nil
 
 function net.init(channel)
+  if not modem then
+    error("No modem found")
+  end
   myChannel = channel
   modem.open(myChannel)
 end
 
 function net.send(to, data)
-  if not myChannel then error("net not initialized") end
+  if not myChannel then
+    error("net not initialized")
+  end
   modem.transmit(MAIN_CHANNEL, myChannel, {
     to = to,
     from = myChannel,
@@ -19,13 +24,14 @@ function net.send(to, data)
 end
 
 function net.receive()
-  if not myChannel then error("net not initialized") end
+  if not myChannel then
+    error("net not initialized")
+  end
   while true do
-    local _, _, ch, reply, msg = os.pullEvent("modem_message")
-    if reply == myChannel and type(msg) == "table" then
+    local _, _, ch, reply, msg, dist = os.pullEvent("modem_message")
+    if ch == myChannel and type(msg) == "table" then
       return msg.from, msg.data
     end
   end
 end
-
 return net
